79f645dbdc5c4b1de15fbcf161e8837dac173500
Marc Burt <marc.burt@gmail.com>
2014-09-16 17:54:20 +0100

    .to_a wasn't needed

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,8 +1,8 @@
 class Hamming
 
   def self.compute (string1, string2)
-    st1 = string1.chars.to_a #returns an array of characters, could also use string1.split(//)
-    st2 = string2.chars.to_a
+    st1 = string1.chars #returns an array of characters, could also use string1.split(//)
+    st2 = string2.chars
     distance = 0
     st1.each_with_index do |string, index|
       distance += 1 if st2[index] && string != st2[index] #increase distance counter if the equivalent index of the second string exists and doesn't match
